b94e849a3aefd04c84cfdf232e5ed15c8f7a35d6
diptnc <chakrabortyd943@gmail.com>
2023-10-21 15:28:33 +0530

    d

--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ export default function Home() {
           priority
         />
       </div>
-      <h1 className="mb-6 text-3xl font-bold">Hello diptanu,</h1>
+      <h1 className="mb-6 text-3xl font-bold">Hello diptanu, !</h1>
       <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
